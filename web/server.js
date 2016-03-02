12af3f9e4aa77b6df6638cbfb98115aa6f15d2a0
JorgeLasaosa <georgelasaosa@gmail.com>
2016-03-02 17:41:35 +0100

    Carpeta public

--- web/server.js
+++ web/server.js
@@ -1,6 +1,12 @@
 var express = require("express");
 	app = express();
 
+
+var login = 'root';
+	password = 'toor';
+
+	mysqli = new mysqli(login, password);
+
 app.use(express.static(__dirname));
 
 app.listen(8000, function() {

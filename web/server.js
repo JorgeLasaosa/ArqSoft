d64336334d994a769f4a239f02e970ef4c64eceb
JorgeLasaosa <georgelasaosa@gmail.com>
2016-03-21 16:04:10 +0100

    server.js

--- web/server.js
+++ web/server.js
@@ -1,13 +1,7 @@
 var express = require("express");
 	app = express();
 
-
-var login = 'root';
-	password = 'toor';
-
-	mysqli = new mysqli(login, password);
-
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(8000, function() {
 	console.log("Server running on port 8000");
